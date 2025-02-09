92126db971778b5ab12cc0492ef38bfba0a15eba
AkashMundari <akashmundari71@gmail.com>
2025-02-09 18:24:54 +0530

    Code Modified 06

--- src/components/PoolAnalysis.jsx
+++ src/components/PoolAnalysis.jsx
@@ -85,6 +85,10 @@ const Pools = () => {
 
   console.log(data);
 
+  const handleDeposit = () => {
+    
+  }
+
   return (
     <>
       <div className="bg-slate-800 h-screen flex-col">
@@ -154,7 +158,7 @@ const Pools = () => {
                 (data.pool.txCount / Number(data.pool.liquidity)) * 1e9}
             </p>
             <div className=" rounded-lg py-6 flex flex-col items-center justify-center gap-4 ">
-              <button className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-md flex items-center font-bold text-xl">
+              <button className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-md flex items-center font-bold text-xl" onClick={handleDeposit}>
                 Deposit <Bitcoin className="ml-2" />
               </button>
               <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 rounded-md font-bold text-xl flex">
