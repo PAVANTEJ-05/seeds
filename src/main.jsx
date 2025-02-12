525b49aac99057621c37e39509d439b32d005775
AvulaRamSwaroop <avularamswaroop@gmail.com>
2025-02-12 14:57:24 +0530

    Hiui

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 const client = new ApolloClient({
   uri: "https://gateway.thegraph.com/api/d962ce4034803ffc53981050cb17a0d2/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV",
@@ -11,12 +11,12 @@ const client = new ApolloClient({
 });
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ToastContainer position="top-right" autoClose={3000} />
     <StrictMode>
       <ApolloProvider client={client}>
         <App />
       </ApolloProvider>
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
