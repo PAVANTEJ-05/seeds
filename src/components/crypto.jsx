1a41dda09bfdfd88a70bc2e7a939996dbebd3e44
PAVANTEJ-05 <nptpavantej79@gmail.com>
2025-03-05 22:55:48 +0530

    added pool sage bot

--- src/components/crypto.jsx
+++ src/components/crypto.jsx
@@ -122,7 +122,7 @@ function CryptoDashboard() {
             
               <input
                 type="text"
-                placeholder="       Search cryptocurrencies..."
+                placeholder="        Search cryptocurrencies..."
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className={`w-full pl-10 pr-4 py-2 rounded-lg border focus:outline-none focus:ring-2 bg-white border-gray-300 text-gray-800 focus:ring-blue-500`}
